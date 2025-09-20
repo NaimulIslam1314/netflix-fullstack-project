1c8cd12fc4ab5dddb02a3289c0c19a68108c13e5
Naimul Islam <nayemhasan1314@gmail.com>
2025-09-21 00:19:12 +0600

    netflix clone

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: "http://localhost:5173", // Use your frontend's actual origin
+    origin: "http://localhost:5173",
     credentials: true
 }));
 
